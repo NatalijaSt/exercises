deae0d22398e420b7351e621d2635ace221c911d
NatalijaSt <natasta@yahoo.com>
2020-02-03 01:47:07 +0100

    some small changes

--- priroda/nature.js
+++ priroda/nature.js
@@ -8,19 +8,19 @@
 var indeksSlike = 1;
 function prethodna(){
     if (indeksSlike == 1) {
-        indeksSlike = 3;//=5 ako imamo 5 slika
+        indeksSlike = 5;//=3 ako imamo 3 slike
     }else{
         indeksSlike--;
     }
-    document.getElementById('dog').src = 
+    document.getElementById('nature').src = 
     'images/priroda' + indeksSlike + ".jfif";//ime foldera,fajla, dok se index sam dodaje
 }
 function sledeca(){
-if (indeksSlike ==3){
+if (indeksSlike ==5){
     indeksSlike = 1;
 }else{
     indeksSlike++;
 }
-document.getElementById('dog').src=
+document.getElementById('nature').src=
 'images/priroda' + indeksSlike +".jfif";
 }
